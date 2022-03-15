5d50130c4844cb8d11899d67b2aa7da7990063b7
Shritesh Bhattarai <shr@ite.sh>
2022-03-15 17:10:47 -0400

    All the materials

--- src/main.zig
+++ src/main.zig
@@ -23,7 +23,10 @@ var first_mouse = true;
 var last_x: f32 = 400.0;
 var last_y: f32 = 300.0;
 
+var frame_counter: usize = 0;
+
 var light_pos = math.f32x4(1.2, 1.0, 2.0, 1.0);
+var current_material: u32 = 0;
 
 pub fn main() !void {
     try glfw.init(.{});
@@ -131,6 +134,13 @@ pub fn main() !void {
         delta_time = current_frame - last_frame;
         last_frame = current_frame;
 
+        frame_counter +%= 1;
+        if (frame_counter % 100 == 0) {
+            current_material += 1;
+            if (current_material == materials.len) current_material = 0;
+            try window.setTitle(materials[current_material].name);
+        }
+
         processInput(window);
 
         gl.clearColor(0.1, 0.1, 0.1, 1.0);
@@ -145,24 +155,14 @@ pub fn main() !void {
 
         lighting_shader.setVec3("viewPos", camera.position);
 
-        lighting_shader.setVec3("material.ambient", .{ 1.0, 0.5, 0.31 });
-        lighting_shader.setVec3("material.diffuse", .{ 1.0, 0.5, 0.32 });
-        lighting_shader.setVec3("material.specular", .{ 0.5, 0.5, 0.5 });
-        lighting_shader.setf32("material.shininess", 32.0);
-
-        const light_color = math.f32x4(
-            math.sin(current_frame * 2.0),
-            math.sin(current_frame * 0.7),
-            math.sin(current_frame * 1.3),
-            1.0,
-        );
-
-        const diffuse_color = math.f32x4s(0.5) * light_color;
-        const ambient_color = math.f32x4s(0.2) * diffuse_color;
+        lighting_shader.setVec3("material.ambient", materials[current_material].ambient);
+        lighting_shader.setVec3("material.diffuse", materials[current_material].diffuse);
+        lighting_shader.setVec3("material.specular", materials[current_material].specular);
+        lighting_shader.setf32("material.shininess", materials[current_material].shininess * 128.0);
 
         lighting_shader.setVec3("light.position", light_pos);
-        lighting_shader.setVec3("light.ambient", ambient_color);
-        lighting_shader.setVec3("light.diffuse", diffuse_color);
+        lighting_shader.setVec3("light.ambient", .{ 1.0, 1.0, 1.0 });
+        lighting_shader.setVec3("light.diffuse", .{ 1.0, 1.0, 1.0 });
         lighting_shader.setVec3("light.specular", .{ 1.0, 1.0, 1.0 });
 
         lighting_shader.setMat("projection", projection);
@@ -235,3 +235,38 @@ fn cursorPosCallback(_: glfw.Window, xpos: f64, ypos: f64) void {
 fn scrollCallback(_: glfw.Window, _: f64, yoffset: f64) void {
     camera.processMouseScroll(@floatCast(f32, yoffset));
 }
+
+const Material = struct {
+    name: [:0]const u8,
+    ambient: math.Vec,
+    diffuse: math.Vec,
+    specular: math.Vec,
+    shininess: f32,
+};
+
+const materials = [_]Material{
+    .{ .name = "emerald", .ambient = .{ 0.0215, 0.1745, 0.0215 }, .diffuse = .{ 0.07568, 0.61424, 0.07568 }, .specular = .{ 0.633, 0.727811, 0.633 }, .shininess = 0.6 },
+    .{ .name = "jade", .ambient = .{ 0.135, 0.2225, 0.1575 }, .diffuse = .{ 0.54, 0.89, 0.63 }, .specular = .{ 0.316228, 0.316228, 0.316228 }, .shininess = 0.1 },
+    .{ .name = "obsidian", .ambient = .{ 0.05375, 0.05, 0.06625 }, .diffuse = .{ 0.18275, 0.17, 0.22525 }, .specular = .{ 0.332741, 0.328634, 0.346435 }, .shininess = 0.3 },
+    .{ .name = "pearl", .ambient = .{ 0.25, 0.20725, 0.20725 }, .diffuse = .{ 1.0, 0.829, 0.829 }, .specular = .{ 0.296648, 0.296648, 0.296648 }, .shininess = 0.088 },
+    .{ .name = "ruby", .ambient = .{ 0.1745, 0.01175, 0.01175 }, .diffuse = .{ 0.61424, 0.04136, 0.04136 }, .specular = .{ 0.727811, 0.626959, 0.626959 }, .shininess = 0.6 },
+    .{ .name = "turquoise", .ambient = .{ 0.1, 0.18725, 0.1745 }, .diffuse = .{ 0.396, 0.74151, 0.69102 }, .specular = .{ 0.297254, 0.30829, 0.306678 }, .shininess = 0.1 },
+    .{ .name = "brass", .ambient = .{ 0.329412, 0.223529, 0.027451 }, .diffuse = .{ 0.780392, 0.568627, 0.113725 }, .specular = .{ 0.992157, 0.941176, 0.807843 }, .shininess = 0.21794872 },
+    .{ .name = "bronze", .ambient = .{ 0.2125, 0.1275, 0.054 }, .diffuse = .{ 0.714, 0.4284, 0.18144 }, .specular = .{ 0.393548, 0.271906, 0.166721 }, .shininess = 0.2 },
+    .{ .name = "chrome", .ambient = .{ 0.25, 0.25, 0.25 }, .diffuse = .{ 0.4, 0.4, 0.4 }, .specular = .{ 0.774597, 0.774597, 0.774597 }, .shininess = 0.6 },
+    .{ .name = "copper", .ambient = .{ 0.19125, 0.0735, 0.0225 }, .diffuse = .{ 0.7038, 0.27048, 0.0828 }, .specular = .{ 0.256777, 0.137622, 0.086014 }, .shininess = 0.1 },
+    .{ .name = "gold", .ambient = .{ 0.24725, 0.1995, 0.0745 }, .diffuse = .{ 0.75164, 0.60648, 0.22648 }, .specular = .{ 0.628281, 0.555802, 0.366065 }, .shininess = 0.4 },
+    .{ .name = "silver", .ambient = .{ 0.19225, 0.19225, 0.19225 }, .diffuse = .{ 0.50754, 0.50754, 0.50754 }, .specular = .{ 0.508273, 0.508273, 0.508273 }, .shininess = 0.4 },
+    .{ .name = "black plastic", .ambient = .{ 0.0, 0.0, 0.0 }, .diffuse = .{ 0.01, 0.01, 0.01 }, .specular = .{ 0.50, 0.50, 0.50 }, .shininess = 0.25 },
+    .{ .name = "cyan plastic", .ambient = .{ 0.0, 0.1, 0.06 }, .diffuse = .{ 0.0, 0.50980392, 0.50980392 }, .specular = .{ 0.50196078, 0.50196078, 0.50196078 }, .shininess = 0.25 },
+    .{ .name = "green plastic", .ambient = .{ 0.0, 0.0, 0.0 }, .diffuse = .{ 0.1, 0.35, 0.1 }, .specular = .{ 0.45, 0.55, 0.45 }, .shininess = 0.25 },
+    .{ .name = "red plastic", .ambient = .{ 0.0, 0.0, 0.0 }, .diffuse = .{ 0.5, 0.0, 0.0 }, .specular = .{ 0.7, 0.6, 0.6 }, .shininess = 0.25 },
+    .{ .name = "white plastic", .ambient = .{ 0.0, 0.0, 0.0 }, .diffuse = .{ 0.55, 0.55, 0.55 }, .specular = .{ 0.70, 0.70, 0.70 }, .shininess = 0.25 },
+    .{ .name = "yellow plastic", .ambient = .{ 0.0, 0.0, 0.0 }, .diffuse = .{ 0.5, 0.5, 0.0 }, .specular = .{ 0.60, 0.60, 0.50 }, .shininess = 0.25 },
+    .{ .name = "black rubber", .ambient = .{ 0.02, 0.02, 0.02 }, .diffuse = .{ 0.01, 0.01, 0.01 }, .specular = .{ 0.4, 0.4, 0.4 }, .shininess = 0.078125 },
+    .{ .name = "cyan rubber", .ambient = .{ 0.0, 0.05, 0.05 }, .diffuse = .{ 0.4, 0.5, 0.5 }, .specular = .{ 0.04, 0.7, 0.7 }, .shininess = 0.078125 },
+    .{ .name = "green rubber", .ambient = .{ 0.0, 0.05, 0.0 }, .diffuse = .{ 0.4, 0.5, 0.4 }, .specular = .{ 0.04, 0.7, 0.04 }, .shininess = 0.078125 },
+    .{ .name = "red rubber", .ambient = .{ 0.05, 0.0, 0.0 }, .diffuse = .{ 0.5, 0.4, 0.4 }, .specular = .{ 0.7, 0.04, 0.04 }, .shininess = 0.078125 },
+    .{ .name = "white rubber", .ambient = .{ 0.05, 0.05, 0.05 }, .diffuse = .{ 0.5, 0.5, 0.5 }, .specular = .{ 0.7, 0.7, 0.7 }, .shininess = 0.078125 },
+    .{ .name = "yellow rubber", .ambient = .{ 0.05, 0.05, 0.0 }, .diffuse = .{ 0.5, 0.5, 0.4 }, .specular = .{ 0.7, 0.7, 0.04 }, .shininess = 0.078125 },
+};
